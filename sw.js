492f215196df1a2ed2e1463ccd5a9b003581fef1
ENDcodeworld <121748084+ENDcodeworld@users.noreply.github.com>
2026-09-22 20:22:29 +0800

    upload: sw.js

---
+++ sw.js
@@ -0,0 +1,51 @@
+/* 躺平 Service Worker：静态资源预缓存 + 运行时缓存，离线可用 */
+const CACHE_STATIC = 'tp-static-v2';
+
+const STATIC_ASSETS = [
+  './',
+  './index.html',
+  './css/style.css',
+  './js/data-products.js',
+  './js/data-articles.js',
+  './js/data-quiz.js',
+  './js/app.js',
+  './manifest.webmanifest',
+  './icons/icon.svg',
+  './icons/icon-192.png',
+  './icons/icon-512.png'
+];
+
+self.addEventListener('install', e => {
+  e.waitUntil(
+    caches.open(CACHE_STATIC).then(c => c.addAll(STATIC_ASSETS)).then(() => self.skipWaiting())
+  );
+});
+
+self.addEventListener('activate', e => {
+  e.waitUntil(
+    caches.keys().then(keys =>
+      Promise.all(keys.filter(k => k !== CACHE_STATIC).map(k => caches.delete(k)))
+    ).then(() => self.clients.claim())
+  );
+});
+
+self.addEventListener('fetch', e => {
+  if (e.request.method !== 'GET') return;
+  const url = new URL(e.request.url);
+  if (url.origin !== location.origin) return;
+
+  // 静态资源：网络优先，离线回退缓存；SPA 导航回退 index.html
+  e.respondWith(
+    fetch(e.request).then(res => {
+      if (res.ok) {
+        const clone = res.clone();
+        caches.open(CACHE_STATIC).then(c => c.put(e.request, clone));
+      }
+      return res;
+    }).catch(() =>
+      caches.match(e.request).then(hit =>
+        hit || (e.request.mode === 'navigate' ? caches.match('./index.html') : Response.error())
+      )
+    )
+  );
+});
